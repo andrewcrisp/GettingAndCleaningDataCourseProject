c99a6152e4ec05367dbbfc399a38668a679c3345
Andrew Crisp <crispa@slhs.org>
2015-06-18 14:24:00 -0600

    Adding documentation

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
-# GettingAndCleaningDataCourseProject
+# GettingAndCleaningDataCourseProject #
+
+## Files ##
+
+run_analysis.R - The single R script to download and process the data.
+README.md - A description of the project and goals.
+CODEBOOK.md - A listing of the variable names in the output data.
+UCI HAR Dataset/ - The original data, associated metadata, readme, and codebook.
+
+## Goals ##
 
 This script will:
-1. Merge training and test data into one data set.
-2. Extract only the mean and standard deviation for each measurement.
-3. Use descriptive names.
-4. Label the data set descriptively.
-5. Create a second, tidy data set with the average of each variable for each activity and subject.
+  1. Merge training and test data into one data set.
+  2. Extract only the mean and standard deviation for each measurement.
+  3. Use descriptive variable names.
+  4. Label the data set descriptively.
+  5. Create a second, tidy data set with the average of each variable for each activity and subject.
+  6. Output that tidy subset to a file, meanOfActivities.txt
+
+These six goals are attained out of order.  
+
+First, we will check for and acquire the original data.  Of the original data files, eight are needed.
+1. Data:
+  * y_train.txt
+  * subject_train.txt
+  * X_train.txt
+  * y_test.txt
+  * subject_test.txt
+  * X_text.txt
+2. Metadata:
+  * activity_labels.txt
+  * features.txt
+  
+Goals - 4, 2
+With the data loaded, we then assign labels and select out only the relevant information.  The labels satisfy goal 4.  Addressing goal 2, only the original observations involving mean() and std() are kept.  The five vectors from the original data that average the signals in the signal windows have been intentionally ignored.  These are observations about the measurements rather than being themselves measurements.
+
+Goal - 1
+After naming the variables, the data is combined into one wide data frame with all variables existing in each row.  Time is taken to further clean up the names and correct typos from the original data set names (specifically, "BodyBody" in a few entries).
+
+Goal - 5
+With clean data available, dplyr is utilized to group and summarize the information into the desired shape.  
+
+Goal - 6
+The data is written to file for storage and transmission.
+
+Goal - 3
+This goal is not found in a specific spot in the script.  Rather, throughout operations, scripting variables are chosen for readability and clarity of purpose.
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,28 @@
+################################################################################
+#
+# Author:   			Andrew Crisp
+# Filename: 			run_analysis.R
+# Comments:			  
+#						      This script will:
+#  					      1. Merge training and test data into one data set.
+#  					      2. Extract only the mean and standard deviation for each measurement.
+#  					      3. Use descriptive variable names.
+#  					      4. Label the data set descriptively.
+#  					      5. Create a second, tidy data set with the average of each variable for each activity and subject.
+#						      6. Output that tidy subset to a file, meanOfActivities.txt
+#  					      
+#    				      Prerequisites: dplyr
+#    				      
+################################################################################
+################################################################################
+### Imports
+################################################################################
+
 library(dplyr)
+
+################################################################################
+### Function declarations
+################################################################################
 
 read.projectFiles.numeric <- function(file, nrows) {
   read.table(
@@ -9,75 +33,84 @@ read.projectFiles.numeric <- function(file, nrows) {
     nrows = nrows)
 }
 
-activityLabels <- read.table(
+################################################################################
+### Script Body
+################################################################################
+################################################################################
+### Goal 0 - Get the data and metadata
+################################################################################
+
+if(!file.exists("UCI HAR Dataset")){
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  destFile <- "UCIDataset.zip"
+  download.file(url=fileUrl, destfile=destFile)
+  unzip("UCIDataset.zip")
+}
+
+yTrain          <- read.projectFiles.numeric(file = "UCI HAR Dataset//train//y_train.txt", nrows = 7352)
+subjectTrain    <- read.projectFiles.numeric(file = "UCI HAR Dataset//train//subject_train.txt", nrows = 7352)
+XTrain          <- read.projectFiles.numeric(file = "UCI HAR Dataset//train//X_train.txt", nrows = 7352)
+yTest           <- read.projectFiles.numeric(file= "UCI HAR Dataset//test//y_test.txt", nrows = 2947)
+subjectTest     <- read.projectFiles.numeric(file = "UCI HAR Dataset//test//subject_test.txt", nrows = 2947)
+XTest           <- read.projectFiles.numeric(file="UCI HAR Dataset//test//X_test.txt", nrows = 2947)
+
+activityLabels  <- read.table(
   file = "UCI HAR Dataset//activity_labels.txt",
   header = FALSE,
   sep = "",
   nrows = 6)
-names(activityLabels) <- c("activityIndex","activityName")
-
-features<-read.table(
+features        <-read.table(
   file = "UCI HAR Dataset//features.txt",
   header = FALSE,
   sep = "",
   nrows = 561)
-columnFilter<-grepl(pattern = "mean\\()|std\\()", x=features[,2])
 
-yTrain<-read.table(
-  file = "UCI HAR Dataset//train//y_train.txt",
-  header = FALSE, 
-  sep = "", 
-  colClasses = "numeric",
-  nrows = 7352)
-subjectTrain<-read.table(
-  file = "UCI HAR Dataset//train//subject_train.txt", 
-  header = FALSE,
-  sep = "", 
-  colClasses = "numeric", 
-  nrows = 7352)
-XTrain<-read.table(
-  file = "UCI HAR Dataset//train//X_train.txt",
-  header = FALSE,
-  sep = "",
-  colClasses="numeric",
-  nrows = 7352)
-names(subjectTrain)<-"subjectID"
-names(yTrain)<-"activityIndex"
-names(XTrain)<-features[,2]
-XTrain<-XTrain[,columnFilter]
-cleanTrain<-cbind(subjectTrain,yTrain,XTrain)
+################################################################################
+### Goal 4 - Label the data set descriptively
+################################################################################
 
-yTest<-read.table(
-  file= "UCI HAR Dataset//test//y_test.txt",
-  header = FALSE,
-  sep="",
-  colClasses="numeric",
-  nrows = 2947)
-subjectTest<-read.table(
-  file = "UCI HAR Dataset//test//subject_test.txt",
-  header = FALSE,
-  sep = "",
-  colClasses="numeric",
-  nrows = 2947)
-XTest<-read.table(
-  file="UCI HAR Dataset//test//X_test.txt",
-  header = FALSE,
-  sep= "",
-  colClasses="numeric",
-  nrows = 2947)
-names(subjectTest)<-"subjectID"
-names(yTest)<-"activityIndex"
-names(XTest)<-features[,2]
-XTest<-XTest[,columnFilter]
-cleanTest<-cbind(subjectTest,yTest,XTest)
+names(subjectTrain)   <- "subjectID"
+names(subjectTest)    <- "subjectID"
+names(yTrain)         <- "activityIndex"
+names(yTest)          <- "activityIndex"
+names(XTrain)         <- features[,2]
+names(XTest)          <- features[,2]
+names(activityLabels) <- c("activityIndex","activityName")
 
-cleanData<-rbind(cleanTrain,cleanTest)
-cleanData<-merge(x=activityLabels,y=cleanData,by="activityIndex")
-cleanData<-select(cleanData,-activityIndex)
+################################################################################
+### Goal 2 - Select only mean and std data, see README.md for reasoning
+################################################################################
+
+columnFilter  <- grepl(pattern = "mean\\()|std\\()", x=features[,2])
+XTrain        <- XTrain[,columnFilter]
+XTest         <- XTest[,columnFilter]
+
+################################################################################
+### Goal 1 - Merge the data into a single data set
+################################################################################
+
+cleanTrain  <- cbind(subjectTrain,yTrain,XTrain)
+cleanTest   <- cbind(subjectTest,yTest,XTest)
+cleanData   <- bind_rows(cleanTrain,cleanTest)
+cleanData   <- merge(x=activityLabels,y=cleanData,by="activityIndex")
+cleanData   <- select(cleanData,-activityIndex)
+
+names(cleanData) <- gsub(pattern = "\\()", replacement = "", names(cleanData))
+names(cleanData) <- gsub(pattern = "BodyBody", replacement = "Body", names(cleanData))
+names(cleanData) <- gsub(pattern = "-", replacement = ".", names(cleanData))
+
+################################################################################
+### Goal 5 - Select and mutate the data into a tidy summary of the average of
+###           each subject and activity
+################################################################################
 
 meanOfActivities <- cleanData %>%
   tbl_df %>%
   group_by(subjectID,activityName) %>%
   summarise_each(funs(mean))
+
+################################################################################
+### Goal 6 - Write to file
+################################################################################
 
 write.table(meanOfActivities,file = 'meanOfActivities.txt',row.name=FALSE)
